86ec66354a89b693bd69c23b244a47aaf4419005
zq992045024 <992045024@qq.com>
2019-08-29 21:45:20 +0800

    第一次上传

---
+++ src/pages/mine/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+<<<<<<< HEAD
+
+=======
+import Usermine from "@components/user"
+>>>>>>> zhangqi
+export default class Mine extends Component {
+    render() {
+        return (
+            <div>
+<<<<<<< HEAD
+                mine
+=======
+                <Usermine/>
+>>>>>>> zhangqi
+            </div>
+        )
+    }
+}
